793b541e912622770be7d3eea076024e5407df0d
m-housni <mohamedhousni1000k@gmail.com>
2024-12-25 14:31:31 +0100

    add HousingLocation interface to define structure for housing location data

---
+++ src/app/housinglocation.ts
@@ -0,0 +1,10 @@
+export interface Housinglocation {
+    id: number,
+    name: string,
+    city: string,
+    state: string,
+    photo: string,
+    availanleUnits: number,
+    wifi: boolean,
+    laundry: boolean,
+}
