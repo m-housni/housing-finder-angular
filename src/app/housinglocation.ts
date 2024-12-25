9b7dc5d6d8f23657868517c05f71caf203a74326
m-housni <mohamedhousni1000k@gmail.com>
2024-12-25 15:11:33 +0100

    update HousingLocation interface and component for consistent naming and add housing location data to HomeComponent

--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -1,10 +1,10 @@
-export interface Housinglocation {
+export interface HousingLocation {
     id: number,
     name: string,
     city: string,
     state: string,
     photo: string,
-    availanleUnits: number,
+    availableUnits: number,
     wifi: boolean,
     laundry: boolean,
 }
